dd9f02468e1dab854ff1900dd45a5da9e0326803
Stefan Helmert <stefan@entroserv.de>
2024-11-27 20:35:51 +0100

    added Freecad file to compare case to PCB; bottom case nearly ready

--- PlugCase/plug_case_bottom.py
+++ PlugCase/plug_case_bottom.py
@@ -22,14 +22,15 @@ def f(x, y, z):
     
 
 
-    mcbtm = bd.fz_cuboid((x+8.0,ya,z-6), (40.,20.,14.8),0.5)
+    mcbtm = bd.fz_cuboid((x+8.0,ya,z-6), (33.,20.,14.8),0.5)
     ccbtm = bd.fz_cuboid((x+10.0,ya,z+3), (42.,40.,24.8),0.5)
-    bodybtm = bd.fz_cuboid((x+9.5,ya,z-5), (50., 31.,25.), 0.5)
+    bodybtm = bd.fz_cuboid((x+9.5,ya,z-5), (48., 31.,25.), 0.5)
     pcbtm = bd.fz_cuboid((x+22,ya,z-7.4), (8.,30.,16.5),0.5)
     scbtm = bd.fz_cuboid((x-5,ya,z-6.4), (8.,29.,16.5),0.5)
+    conhole = bd.fz_circle((min(z,-2)+2+max(z,4)-4,max(ya,7-z/5)-7+z/5),1.5)
 
 
-    bottom = cmb.fz_and_chamfer(0.5, bodybtm, -top, -mcbtm, -ccbtm, -pcbtm, -scbtm, -hole)
+    bottom = cmb.fz_and_chamfer(0.5, bodybtm, -top, -mcbtm, -ccbtm, -pcbtm, -scbtm, -conhole, -hole)
 
 
     if bottom > 0:
